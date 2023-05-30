a2bac4b581d3caf47737242dd616563ccf98a56e
Anoureen-walid <anoureen.walid@icloud.com>
2023-05-29 19:47:47 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,21 +6,20 @@
  *
  * Return: the int converted from the string
  */
-int -atoi(char *s)
+int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
-
+	int i, d, n, l, f, digit;
 	i = 0;
 	d = 0;
 	n = 0;
-	len = 0;
+	l = 0;
 	f = 0;
 	digit = 0;
 
-	while (s[len] != '\0')
-		len++;
+	while (s[l] != '\0')
+		l++;
 
-	while (i < len && f == 0)
+	while (i < l && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
